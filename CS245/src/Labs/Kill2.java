a5fb6837f4e749f7f0c108d66f49ad3b715160e1
nickschumann27 <nickschumann27@10.35.2.51>
2020-02-05 22:49:29 -0600

    Process

--- CS245/src/Labs/Kill2.java
+++ CS245/src/Labs/Kill2.java
@@ -1,18 +1,28 @@
 package Labs;
 
 public class Kill2 {
+	
 	public static void main(String[] args) {
-		System.out.println(remove2nd("cat dog"));
+	
 		if(args.length != 1) {
+			
 			System.out.println("Invalid number - only accepting one");
+			System.exit(0);
+			
 		}
+		remove2nd(args[0]);
 	}
+	
+	
 	public static String remove2nd(String s) {
 		int length = s.length();
-		if(length > 2) {
+		if(length > 1) {
 			return s.charAt(0) + s.substring(2);
 		}
 		return "error";
+		
+		// s.substring(0,1)
+		
 	}
 
 
